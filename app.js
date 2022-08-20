bcaa9435f3f569a3e932b9cdb33ce96381138890
Jrevoredo42 <revoredojf@gmail.com>
2022-08-20 01:20:15 -0300

    foi criado conexao com o mongodb utilizando mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const server = express();
@@ -6,5 +7,19 @@ const server = express();
 server.listen(process.env.PORT, () => {
     console.log('Server is running')
 })
+
+mongoose.connect(process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if(err) {
+        console.log(err);
+        }else{
+        console.log('connected to mongoDB')
+        }
+    })
+
 
 module.exports = server;
