332cd906d34177391ccf391a67498bde168fe0f8
Jrevoredo42 <revoredojf@gmail.com>
2022-08-20 00:46:57 -0300

    foi inciado ambiente node com dependencias do mongodb, juntamente da inicializacao do servidor no arquivo app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const server = express();
+
+server.listen(process.env.PORT, () => {
+    console.log('Server is running')
+})
+
+module.exports = server;
